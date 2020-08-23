1c0d4d9fbf0611f88ed00a0260a0d51010e3deb8
Jared Jensen <jaredjxyz@gmail.com>
2020-08-22 18:10:46 -0700

    Add borrowing.h

---
+++ include/borrowing/borrowing.h
@@ -0,0 +1,56 @@
+#include <memory>
+#include <condition_variable>
+#include <memory>
+#include <thread>
+#include <iostream>
+
+namespace borrowing {
+
+template<class T>
+class Borrowable;
+
+template <class T>
+class Borrowed {
+
+  friend class Borrowable<T>;
+
+  T* obj_ptr_;
+  std::unique_lock<std::mutex> lock_;
+
+  Borrowed(T* obj_ptr, std::unique_lock<std::mutex>&& lock)
+      : obj_ptr_(obj_ptr), lock_(std::move(lock)) {}
+
+
+ public:
+  T* operator->() const { return obj_ptr_; }
+  T& operator*() const { return *obj_ptr_; }
+};
+
+
+template <class T>
+class Borrowable {
+  std::unique_ptr<T> obj_;
+  std::mutex mutex_;
+
+ public:
+  Borrowable(std::unique_ptr<T>&& obj) : obj_(std::move(obj)) {}
+  Borrowable(const T& obj) : obj_(std::make_unique<T>(obj)) {};
+  Borrowable(T&& obj) : obj_(std::make_unique<T>(std::move(obj))) {}
+
+  Borrowed<T> borrow() {
+    std::unique_lock<std::mutex> lock(mutex_);
+    return Borrowed<T>(obj_.get(), std::move(lock));
+  }
+};
+
+struct X {
+  int i;
+  X(int i) : i(i) {}
+  X(const X& other) = default;
+  X& operator=(const X& other) {
+    i = other.i;
+    return *this;
+  }
+};
+
+}  // namespace borrowing
